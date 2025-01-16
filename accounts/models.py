ba4a2b95c24a42ce1b41091a38b73838a3aa6402
Md-Merazul-Islam <mdmerazul75@gmail.com>
2025-01-16 17:16:24 +0600

    initialize accounts app with basic structure and models

---
+++ accounts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+
+    # Avoid conflicts by setting unique related_name values
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="customuser_set",
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="customuser_permissions_set",
+        blank=True
+    )
